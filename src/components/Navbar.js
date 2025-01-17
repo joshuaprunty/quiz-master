2a8f17cdd8a9e982abdae2902a36e400cd9bf903
Josh Prunty <joshprunty@gmail.com>
2025-01-16 18:40:35 -0600

    small cleanups

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar() {
                   <NavigationMenuLink className={cn(
                     "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50"
                   )}>
-                    Test Page
+                    About
                   </NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
