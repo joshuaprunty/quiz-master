fb0394440bf852bde73ad438d4421501e70c7d7a
Josh Prunty <joshprunty@gmail.com>
2025-01-19 18:31:34 -0600

    added shadcn select component

--- src/app/(authenticated)/dashboard/create/settings/page.js
+++ src/app/(authenticated)/dashboard/create/settings/page.js
@@ -10,6 +10,14 @@ import {
 import { Button } from "@/components/ui/button"
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 
 export default function Create() {
@@ -17,6 +25,28 @@ export default function Create() {
     <div className="p-8">
       <h1 className="text-3xl font-bold">Quiz Settings</h1>
       <p className="mt-4">Set the quiz settings</p>
+      <p className="mt-4">Setting one</p>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <p className="mt-4">Setting two</p>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
       <Button className="w-full max-w-7xl my-12">
         <Link href="/dashboard/create/edit">
           Next
