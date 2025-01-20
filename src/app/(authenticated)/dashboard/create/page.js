3bfbc35ab74398c4ec0175268d6d5ffcbfe67f65
Josh Prunty <joshprunty@gmail.com>
2025-01-19 18:17:27 -0600

    trying out file uploader package

--- src/app/(authenticated)/dashboard/create/page.js
+++ src/app/(authenticated)/dashboard/create/page.js
@@ -1,8 +1,11 @@
+import FileUpload from "@/components/FileUpload";
+
 export default function Create() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">Create Quiz</h1>
       <p className="mt-4">Initialize quiz generation</p>
+      <FileUpload />
     </div>
   )
 }
